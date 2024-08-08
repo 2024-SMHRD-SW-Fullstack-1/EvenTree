cb9f03ba964225492e0aff29f92c8c9b4c65ff01
김재효 <67933778+JaehyoDev@users.noreply.github.com>
2024-08-08 11:46:43 +0900

    Delete app directory

--- app/src/main/java/com/example/andhack/EventVO.kt
+++
@@ -1,4 +0,0 @@
-package com.example.andhack
-
-
-data class EventVO(val title: String, val writer: String, val startDate: String, val endDate: String, val content: String)
--- app/src/main/java/com/example/andhack/MemberVO.kt
+++
@@ -1,3 +0,0 @@
-package com.example.andhack
-
-data class MemberVO(var id: String, var pw: String, var email: String?)
